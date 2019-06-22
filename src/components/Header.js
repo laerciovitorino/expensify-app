114ec027f980073d3de15e3d733a0a10340b9771
Laércio Vitorino <laerciovitorino@gmail.com>
2019-06-22 18:44:17 -0300

    Add URL parameters to the Edit component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
     <h1>Expensify</h1>
     <p><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></p>
     <p><NavLink to="/create" activeClassName="is-active">Create Expense</NavLink></p>
-    <p><NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink></p>
     <p><NavLink to="/help" activeClassName="is-active">Help</NavLink></p>
   </header>
 );
