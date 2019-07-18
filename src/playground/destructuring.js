6fc646b56e663f22ab3b81078fb5c52b1f5cd113
Laércio Vitorino <laerciovitorino@gmail.com>
2019-07-18 11:28:28 -0300

    Practicing ES6 array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,27 +1,43 @@
-const person = {
-  name: 'Laércio',
-  age: 32,
-  location: {
-    city: 'Boa Vista',
-    temp: 22
-  }
-};
+//
+// Object destructuring
+//
 
-const { name: firstName = 'Anonymous', age } = person;
-console.log(`${firstName} is ${age}.`);
+// const person = {
+//   name: 'Laércio',
+//   age: 32,
+//   location: {
+//     city: 'Boa Vista',
+//     temp: 22
+//   }
+// };
 
-const { city, temp: temperature } = person.location;
-if (city && temperature) {
-  console.log(`It's ${temperature} in ${city}.`);
-}
+// const { name: firstName = 'Anonymous', age } = person;
+// console.log(`${firstName} is ${age}.`);
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-};
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//   console.log(`It's ${temperature} in ${city}.`);
+// }
 
-const { name: publisherName = 'Self-Published' } = book.publisher;
-console.log(publisherName);
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
+
+// const { name: publisherName = 'Self-Published' } = book.publisher;
+// console.log(publisherName);
+
+//
+// Array destructuring
+//
+
+// const address = ['87 Bom Jesus Street', 'Boa Vista', 'Paraíba', '58123-000'];
+// const [, city, state = 'Pernambuco'] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}.`);
